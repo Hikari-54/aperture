baf2209a131bedd1f760c29905c7f5760c7676a5
Hikari-54 <climberte@yandex.ru>
2023-08-25 19:30:34 +0300

    area grid in process

--- src/components/Areas/Areas.jsx
+++ src/components/Areas/Areas.jsx
@@ -7,20 +7,111 @@ export default function Areas() {
                 <h2>What we do.</h2>
                 <h4>The areas that we're specialized in.</h4>
             </div>
-            <div className="areas__grid">
-                <div className="areas__product">
-                    <h5></h5>
+                <div className="areas__grid-container">
+                    <div className="areas__grid-frame">
+                        <div className="areas__element areas__product">
+                            <div className="areas__inner-egde">
+                                <h5>Product Photography</h5>
+                                <p>Cras commodo consequat orci, in convallis risus egestas non. Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus libero ac, cursus quam.</p>
+                                <button className='areas__button'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                                        <g clip-path="url(#clip0_2_229)">
+                                            <path d="M16 5.51392C21.7915 5.51392 26.4861 10.2085 26.4861 16C26.4861 21.7915 21.7915 26.4861 16 26.4861C10.2085 26.4861 5.51389 21.7915 5.51389 16C5.51389 10.2085 10.2085 5.51392 16 5.51392Z" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M18.4223 11.8057L24.4413 22.2289" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.5777 11.8057H25.6158" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M11.1554 16.0001L17.1744 5.5769" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.5777 20.1944L7.55868 9.77124" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M18.4223 20.1946H6.38423" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M20.8446 16L14.8256 26.4232" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_2_229">
+                                            <rect width="32" height="32" fill="white"/>
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                    <span className='areas__button-caption'>Read more</span>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="areas__element areas__architecture">
+                            <div className="areas__inner-egde">
+                                <h5>Architecture Photography</h5>
+                                <p>Aenean porta neque eget consequat fringilla. Vestibulum ultrices, orci nec egestas pharetra, ligula est semper enim, nec auctor sapien leo nec purus. Fusce tincidunt aliquet sapien, sit amet rhoncus leo imperdiet nec.</p>
+                                <button className='areas__button'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                                        <g clip-path="url(#clip0_2_229)">
+                                            <path d="M16 5.51392C21.7915 5.51392 26.4861 10.2085 26.4861 16C26.4861 21.7915 21.7915 26.4861 16 26.4861C10.2085 26.4861 5.51389 21.7915 5.51389 16C5.51389 10.2085 10.2085 5.51392 16 5.51392Z" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M18.4223 11.8057L24.4413 22.2289" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.5777 11.8057H25.6158" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M11.1554 16.0001L17.1744 5.5769" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.5777 20.1944L7.55868 9.77124" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M18.4223 20.1946H6.38423" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M20.8446 16L14.8256 26.4232" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_2_229">
+                                            <rect width="32" height="32" fill="white"/>
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                    <span className='areas__button-caption'>Read more</span>
+                                </button>
+                            </div>                
+                        </div>
+                        <div className="areas__element areas__drone">
+                            <div className="areas__inner-egde">
+                                <h5>Drone Photography</h5>
+                                <p>Mauris euismod elit et nisi ultrices, ut faucibus orci tincidunt. Duis tristique sed lorem a vestibulum. Cras commodo consequat orci, in convallis risus egestas non. Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus libero ac, cursus quam.</p>
+                                <button className='areas__button'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                                        <g clip-path="url(#clip0_2_229)">
+                                            <path d="M16 5.51392C21.7915 5.51392 26.4861 10.2085 26.4861 16C26.4861 21.7915 21.7915 26.4861 16 26.4861C10.2085 26.4861 5.51389 21.7915 5.51389 16C5.51389 10.2085 10.2085 5.51392 16 5.51392Z" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M18.4223 11.8057L24.4413 22.2289" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.5777 11.8057H25.6158" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M11.1554 16.0001L17.1744 5.5769" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.5777 20.1944L7.55868 9.77124" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M18.4223 20.1946H6.38423" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M20.8446 16L14.8256 26.4232" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_2_229">
+                                            <rect width="32" height="32" fill="white"/>
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                    <span className='areas__button-caption'>Read more</span>
+                                </button>
+                            </div>                            
+                        </div>
+                        <div className="areas__element areas__wildlife">
+                            <div className="areas__inner-egde">
+                                <h5>Wildlife Photography</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque aliquam odio et faucibus. Nulla rhoncus feugiat eros quis consectetur. Morbi neque ex, condimentum dapibus congue et, vulputate ut ligula. Vestibulum sit amet urna turpis.</p>
+                                <button className='areas__button'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                                        <g clip-path="url(#clip0_2_229)">
+                                            <path d="M16 5.51392C21.7915 5.51392 26.4861 10.2085 26.4861 16C26.4861 21.7915 21.7915 26.4861 16 26.4861C10.2085 26.4861 5.51389 21.7915 5.51389 16C5.51389 10.2085 10.2085 5.51392 16 5.51392Z" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M18.4223 11.8057L24.4413 22.2289" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.5777 11.8057H25.6158" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M11.1554 16.0001L17.1744 5.5769" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.5777 20.1944L7.55868 9.77124" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M18.4223 20.1946H6.38423" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M20.8446 16L14.8256 26.4232" stroke="black" stroke-width="12.5833" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_2_229">
+                                            <rect width="32" height="32" fill="white"/>
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                    <span className='areas__button-caption'>Read more</span>
+                                </button>
+                            </div>                            
+                        </div>
+                    </div>
+                    
                 </div>
-                <div className="areas__architecture">
-
-                </div>
-                <div className="areas__drone">
-
-                </div>
-                <div className="areas__wildlife">
-
-                </div>
-            </div>
         </section>
     )
 }
